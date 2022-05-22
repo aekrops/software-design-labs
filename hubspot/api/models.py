bafd1d50f0bac15abbb9e65a365181e6136598d4
Sviatoslav Trostynskyi <svyatoslavtrostynski24@gmail.com>
2022-05-22 03:37:15 +0300

    implemented User | REST

--- hubspot/api/models.py
+++ hubspot/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self):
+        return self.email
+
--- hubspot/api/views.py
+++ hubspot/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class ListUserView(generics.ListAPIView):
+    """
+    Provides a get method handler
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
