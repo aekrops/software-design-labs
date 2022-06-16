84e93ab0df453497dc84e6511f1ca60585caa2f0
Sviatoslav Trostynskyi <svyatoslavtrostynski24@gmail.com>
2022-06-16 04:38:22 +0300

    update admin

--- hubspot/api/admin.py
+++ hubspot/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Client, Conversation, Message
 
 
 admin.site.register(User)
+admin.site.register(Client)
+admin.site.register(Conversation)
+admin.site.register(Message)
